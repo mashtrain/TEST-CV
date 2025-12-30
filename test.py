ded4484f619b11341c0e672da360e63e8377d9fd
mashtrain <alamsyahlumajang321@gmail.com>
2025-12-30 12:59:49 +0700

    update deteksi hijau

--- test.py
+++ test.py
@@ -10,19 +10,19 @@ while True:
         break
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     
-    lower_red = np.array([0, 120, 70])
-    upper_red = np.array([10,255,255])
+    lower_red = np.array([40, 70, 70])
+    upper_red = np.array([80,255,255])
     
-    lower_red2 = np.array([170,120,70])
-    upper_red2 = np.array([180,255,255])
+    # lower_red2 = np.array([170,120,70])
+    # upper_red2 = np.array([180,255,255])
     
     mask1 = cv2.inRange(hsv, lower_red, upper_red)
-    mask2 = cv2.inRange(hsv,lower_red2, upper_red2)
+    # mask2 = cv2.inRange(hsv,lower_red2, upper_red2)
     
-    mask = mask1 + mask2
+    # mask = mask1 + mask2
     
     cv2.imshow("kamera asli", frame)
-    cv2.imshow("Hanya merah", mask)
+    cv2.imshow("Hanya merah", mask1)
     
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
